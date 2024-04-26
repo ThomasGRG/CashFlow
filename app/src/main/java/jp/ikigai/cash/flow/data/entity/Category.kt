b500cd3c1423d9f21ff899ce017f785fc7928ef8
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2024-04-26 08:44:43 +0530

    Add category entity

---
+++ app/src/main/java/jp/ikigai/cash/flow/data/entity/Category.kt
@@ -0,0 +1,14 @@
+package jp.ikigai.cash.flow.data.entity
+
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+import org.mongodb.kbson.ObjectId
+
+class Category : RealmObject {
+    @PrimaryKey
+    var _id: ObjectId = ObjectId()
+    var name: String = ""
+    var iconName: String = ""
+    var frequency: Int = 0
+    var lastUsed: Long = 0L
+}
