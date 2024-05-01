bed59162cfa70957be78d3b751845d77c6c09301
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2024-05-01 23:31:03 +0530

    Add compose utils

---
+++ app/src/main/java/jp/ikigai/cash/flow/utils/ComposeUtils.kt
@@ -0,0 +1,61 @@
+package jp.ikigai.cash.flow.utils
+
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavDeepLink
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import jp.ikigai.cash.flow.data.Constants
+
+val TextFieldValueSaver = Saver<MutableState<TextFieldValue>, String>(
+    save = { it.component1().text },
+    restore = { mutableStateOf(TextFieldValue(it, TextRange(it.length))) }
+)
+
+fun NavGraphBuilder.animatedComposable(
+    route: String,
+    arguments: List<NamedNavArgument> = emptyList(),
+    deepLinks: List<NavDeepLink> = emptyList(),
+    content: @Composable AnimatedContentScope.(NavBackStackEntry) -> Unit
+) {
+    composable(
+        route = route,
+        arguments = arguments,
+        deepLinks = deepLinks,
+        enterTransition = {
+            slideIntoContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
+                animationSpec = tween(Constants.tweenDuration)
+            )
+        },
+        exitTransition = {
+            slideOutOfContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
+                animationSpec = tween(Constants.tweenDuration)
+            )
+        },
+        popEnterTransition = {
+            slideIntoContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Companion.Right,
+                animationSpec = tween(Constants.tweenDuration)
+            )
+        },
+        popExitTransition = {
+            slideOutOfContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Companion.Right,
+                animationSpec = tween(Constants.tweenDuration)
+            )
+        }
+    ) {
+        content(this, it)
+    }
+}
