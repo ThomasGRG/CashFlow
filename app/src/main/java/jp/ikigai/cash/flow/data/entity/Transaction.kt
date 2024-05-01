923c5e3198f7fb6add147eef0113315d22fb7d5c
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2024-05-01 23:28:42 +0530

    Change primary key to uuid, add getter and setter for type enum, change realmSet to realmList and add no args constructor

--- app/src/main/java/jp/ikigai/cash/flow/data/entity/Transaction.kt
+++ app/src/main/java/jp/ikigai/cash/flow/data/entity/Transaction.kt
@@ -1,29 +1,40 @@
 package jp.ikigai.cash.flow.data.entity
 
 import android.icu.util.Currency
-import io.realm.kotlin.ext.realmSetOf
+import io.realm.kotlin.ext.realmListOf
+import io.realm.kotlin.types.RealmList
 import io.realm.kotlin.types.RealmObject
-import io.realm.kotlin.types.RealmSet
 import io.realm.kotlin.types.annotations.PrimaryKey
 import jp.ikigai.cash.flow.data.enums.TransactionType
 import jp.ikigai.cash.flow.utils.toEpochMilli
-import org.mongodb.kbson.ObjectId
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
-class Transaction : RealmObject {
+class Transaction() : RealmObject {
     @PrimaryKey
-    var _id: ObjectId = ObjectId()
+    var uuid: String = ""
     var title: String = ""
     var description: String = ""
     var amount: Double = 0.0
     var taxAmount: Double = 0.0
-    var type: Int = TransactionType.DEBIT.id
+
+    private var typeId: Int = TransactionType.DEBIT.id
+    var type: TransactionType
+        get() {
+            for (type in TransactionType.values()) {
+                if (type.id == typeId) return type
+            }
+            return TransactionType.DEBIT
+        }
+        set(value) {
+            typeId = value.id
+        }
+
     var currency: String = Currency.getInstance("INR").currencyCode
     var time: Long = ZonedDateTime.now(ZoneId.of("UTC")).toEpochMilli()
     var category: Category? = null
     var counterParty: CounterParty? = null
     var method: Method? = null
     var source: Source? = null
-    var items: RealmSet<TransactionItem> = realmSetOf()
+    var items: RealmList<TransactionItem> = realmListOf()
 }
