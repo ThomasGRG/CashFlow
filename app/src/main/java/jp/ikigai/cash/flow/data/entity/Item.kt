e6e5135c374bec5373ac00b2c60b2acc2daefff5
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2024-05-01 23:27:48 +0530

    Change primary key to uuid, add getter and setter for last used unit and add no args constructor

--- app/src/main/java/jp/ikigai/cash/flow/data/entity/Item.kt
+++ app/src/main/java/jp/ikigai/cash/flow/data/entity/Item.kt
@@ -3,13 +3,22 @@ package jp.ikigai.cash.flow.data.entity
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
 import jp.ikigai.cash.flow.data.enums.ItemUnit
-import org.mongodb.kbson.ObjectId
 
-class Item : RealmObject {
+class Item() : RealmObject {
     @PrimaryKey
-    var _id: ObjectId = ObjectId()
+    var uuid: String = ""
     var name: String = ""
-    var lastUsedUnit: Int = ItemUnit.PIECE.id
+    private var lastUsedUnitId: Int = ItemUnit.PIECE.id
+    var lastUsedUnit: ItemUnit
+        get() {
+            for (unit in ItemUnit.values()) {
+                if (unit.id == lastUsedUnitId) return unit
+            }
+            return ItemUnit.PIECE
+        }
+        set(value) {
+            lastUsedUnitId = value.id
+        }
     var lastUsedCurrency: String = ""
     var lastKnownPrice: Double = 0.0
     var frequency: Int = 0
