6c44d524e465187cefa1bacd7b558c405417e38d
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2024-05-01 23:35:13 +0530

    Add sheet types

---
+++ app/src/main/java/jp/ikigai/cash/flow/data/enums/SheetType.kt
@@ -0,0 +1,18 @@
+package jp.ikigai.cash.flow.data.enums
+
+enum class SheetType {
+    NONE,
+    DATE,
+    TIME,
+    CATEGORY,
+    COUNTERPARTY,
+    METHOD,
+    SOURCE,
+    TYPE,
+    CURRENCY,
+    DATE_RANGE,
+    MORE_OPTIONS,
+    FILTER,
+    TEMPLATES,
+    ITEMS
+}
