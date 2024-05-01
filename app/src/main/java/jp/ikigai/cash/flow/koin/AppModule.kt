b24c2f185c93abf8ef970d04d3dd99ac32346d10
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2024-05-01 23:43:30 +0530

    Add viewModels for dependency injection

--- app/src/main/java/jp/ikigai/cash/flow/koin/AppModule.kt
+++ app/src/main/java/jp/ikigai/cash/flow/koin/AppModule.kt
@@ -1,5 +1,36 @@
 package jp.ikigai.cash.flow.koin
 
+import jp.ikigai.cash.flow.ui.viewmodels.common.ChooseIconScreenViewModel
+import jp.ikigai.cash.flow.ui.viewmodels.listing.CategoryScreenViewModel
+import jp.ikigai.cash.flow.ui.viewmodels.listing.CounterPartyScreenViewModel
+import jp.ikigai.cash.flow.ui.viewmodels.listing.ItemsScreenViewModel
+import jp.ikigai.cash.flow.ui.viewmodels.listing.MethodScreenViewModel
+import jp.ikigai.cash.flow.ui.viewmodels.listing.SourceScreenViewModel
+import jp.ikigai.cash.flow.ui.viewmodels.listing.TransactionsScreenViewModel
+import jp.ikigai.cash.flow.ui.viewmodels.upsert.UpsertCategoryScreenViewModel
+import jp.ikigai.cash.flow.ui.viewmodels.upsert.UpsertCounterPartyScreenViewModel
+import jp.ikigai.cash.flow.ui.viewmodels.upsert.UpsertMethodScreenViewModel
+import jp.ikigai.cash.flow.ui.viewmodels.upsert.UpsertSourceScreenViewModel
+import jp.ikigai.cash.flow.ui.viewmodels.upsert.UpsertTransactionScreenViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val appModule = module {  }
+val appModule = module {
+    viewModel() { TransactionsScreenViewModel() }
+    viewModel() { UpsertTransactionScreenViewModel(get()) }
+
+    viewModel() { CounterPartyScreenViewModel() }
+    viewModel() { UpsertCounterPartyScreenViewModel(get()) }
+
+    viewModel() { SourceScreenViewModel() }
+    viewModel() { UpsertSourceScreenViewModel(get()) }
+
+    viewModel() { MethodScreenViewModel() }
+    viewModel() { UpsertMethodScreenViewModel(get()) }
+
+    viewModel() { CategoryScreenViewModel() }
+    viewModel() { UpsertCategoryScreenViewModel(get()) }
+
+    viewModel() { ChooseIconScreenViewModel(get()) }
+    viewModel() { ItemsScreenViewModel() }
+}
