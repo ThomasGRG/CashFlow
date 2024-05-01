7eba83ca53113456c1f0aad1ea8ed3291f899bd9
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2024-05-01 23:32:00 +0530

    Add events

---
+++ app/src/main/java/jp/ikigai/cash/flow/data/Event.kt
@@ -0,0 +1,15 @@
+package jp.ikigai.cash.flow.data
+
+sealed class Event {
+    object InternalError: Event()
+    object SaveSuccess : Event()
+    object DeleteSuccess : Event()
+    object NotEnoughBalance : Event()
+    object SourceCategoryMethodRequired : Event()
+    object CategoryMethodRequired : Event()
+    object SourceCategoryRequired : Event()
+    object SourceMethodRequired : Event()
+    object CategoryRequired : Event()
+    object MethodRequired : Event()
+    object SourceRequired : Event()
+}
