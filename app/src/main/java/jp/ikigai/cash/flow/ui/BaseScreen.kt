17fbf227a015400478a54e2ea691628c8d616b2b
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2024-05-05 17:42:05 +0530

    Add transaction template screens to base screen

--- app/src/main/java/jp/ikigai/cash/flow/ui/BaseScreen.kt
+++ app/src/main/java/jp/ikigai/cash/flow/ui/BaseScreen.kt
@@ -6,18 +6,20 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import jp.ikigai.cash.flow.data.Routes
-import jp.ikigai.cash.flow.ui.screens.listing.categoryScreen
-import jp.ikigai.cash.flow.ui.screens.listing.transactionsScreen
 import jp.ikigai.cash.flow.ui.screens.common.chooseIconScreen
+import jp.ikigai.cash.flow.ui.screens.listing.categoryScreen
 import jp.ikigai.cash.flow.ui.screens.listing.counterPartyScreen
 import jp.ikigai.cash.flow.ui.screens.listing.itemScreen
 import jp.ikigai.cash.flow.ui.screens.listing.methodScreen
 import jp.ikigai.cash.flow.ui.screens.listing.sourceScreen
+import jp.ikigai.cash.flow.ui.screens.listing.transactionTemplateScreen
+import jp.ikigai.cash.flow.ui.screens.listing.transactionsScreen
 import jp.ikigai.cash.flow.ui.screens.upsert.upsertCategoryScreen
 import jp.ikigai.cash.flow.ui.screens.upsert.upsertCounterPartyScreen
 import jp.ikigai.cash.flow.ui.screens.upsert.upsertMethodScreen
 import jp.ikigai.cash.flow.ui.screens.upsert.upsertSourceScreen
 import jp.ikigai.cash.flow.ui.screens.upsert.upsertTransactionScreen
+import jp.ikigai.cash.flow.ui.screens.upsert.upsertTransactionTemplateScreen
 
 @Composable
 fun BaseScreen() {
@@ -30,6 +32,9 @@ fun BaseScreen() {
     ) {
         transactionsScreen(navController = navController)
         upsertTransactionScreen(navController = navController)
+
+        transactionTemplateScreen(navController = navController)
+        upsertTransactionTemplateScreen(navController = navController)
 
         chooseIconScreen(navController = navController)
 
