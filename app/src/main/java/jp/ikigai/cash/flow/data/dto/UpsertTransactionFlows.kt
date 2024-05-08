6db20dea7f2d15a4b07c0f77911bd0b9a6bda688
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2024-05-08 08:49:12 +0530

    Add UpsertTransactionFlows dto

---
+++ app/src/main/java/jp/ikigai/cash/flow/data/dto/UpsertTransactionFlows.kt
@@ -0,0 +1,20 @@
+package jp.ikigai.cash.flow.data.dto
+
+import jp.ikigai.cash.flow.data.entity.Category
+import jp.ikigai.cash.flow.data.entity.CounterParty
+import jp.ikigai.cash.flow.data.entity.Item
+import jp.ikigai.cash.flow.data.entity.Method
+import jp.ikigai.cash.flow.data.entity.Source
+import jp.ikigai.cash.flow.data.entity.Transaction
+import jp.ikigai.cash.flow.data.entity.TransactionTemplate
+
+data class UpsertTransactionFlows(
+    val categories: List<Category> = emptyList(),
+    val counterParties: List<CounterParty> = emptyList(),
+    val methods: List<Method> = emptyList(),
+    val sources: List<Source> = emptyList(),
+    val items: List<Item> = emptyList(),
+    val transactionTitles: List<String> = emptyList(),
+    val transaction: Transaction? = null,
+    val transactionTemplate: TransactionTemplate? = null,
+)
