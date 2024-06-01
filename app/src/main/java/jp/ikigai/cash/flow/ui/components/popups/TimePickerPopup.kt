94d93d6deb2f050a13f71477f70bc41a1b96ab89
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2024-06-01 07:49:29 +0530

    Add TimePickerPopup component

---
+++ app/src/main/java/jp/ikigai/cash/flow/ui/components/popups/TimePickerPopup.kt
@@ -0,0 +1,94 @@
+package jp.ikigai.cash.flow.ui.components.popups
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import jp.ikigai.cash.flow.R
+import java.time.ZonedDateTime
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TimePickerPopup(
+    time: ZonedDateTime,
+    updateTime: (ZonedDateTime) -> Unit,
+    dismiss: () -> Unit,
+) {
+    val timePickerState = rememberTimePickerState(
+        is24Hour = false,
+        initialHour = time.hour,
+        initialMinute = time.minute
+    )
+
+    Column(
+        modifier = Modifier
+            .clip(RoundedCornerShape(14.dp))
+            .background(MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp))
+            .padding(top = 24.dp, bottom = 15.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        TimePicker(
+            state = timePickerState
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp, end = 10.dp, bottom = 10.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            OutlinedButton(
+                onClick = {
+                    dismiss()
+                },
+                modifier = Modifier
+                    .weight(1f)
+                    .height(50.dp),
+                shape = RoundedCornerShape(35)
+            ) {
+                Text(text = stringResource(id = R.string.cancel_button_label))
+            }
+            Spacer(modifier = Modifier.width(10.dp))
+            FilledTonalButton(
+                onClick = {
+                    dismiss()
+                    updateTime(time.withHour(timePickerState.hour).withMinute(timePickerState.minute))
+                },
+                modifier = Modifier
+                    .weight(1f)
+                    .height(50.dp),
+                shape = RoundedCornerShape(35),
+            ) {
+                Text(text = stringResource(id = R.string.set_button_label))
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TimePickerPopupPreview() {
+    TimePickerPopup(time = ZonedDateTime.now(), updateTime = {}, dismiss = {})
+}
