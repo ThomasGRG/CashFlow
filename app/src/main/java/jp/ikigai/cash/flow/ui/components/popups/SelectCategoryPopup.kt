30e2e04a9dab9743e5216b4ee993b4c3cb5daacf
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2024-11-13 01:45:51 +0530

    SelectCategoryPopup - Implement searching, search highlighting, add list item animations and improve design

--- app/src/main/java/jp/ikigai/cash/flow/ui/components/popups/SelectCategoryPopup.kt
+++ app/src/main/java/jp/ikigai/cash/flow/ui/components/popups/SelectCategoryPopup.kt
@@ -1,6 +1,9 @@
 package jp.ikigai.cash.flow.ui.components.popups
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,25 +15,37 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import compose.icons.TablerIcons
 import compose.icons.tablericons.Archive
 import jp.ikigai.cash.flow.R
 import jp.ikigai.cash.flow.data.entity.Category
-import jp.ikigai.cash.flow.ui.components.buttons.IconToggleRow
+import jp.ikigai.cash.flow.ui.components.common.SelectableCard
+import jp.ikigai.cash.flow.utils.getHighlightedString
 
 @Composable
 fun SelectCategoryPopup(
@@ -41,11 +56,55 @@ fun SelectCategoryPopup(
     dismiss: () -> Unit,
 ) {
     val haptics = LocalHapticFeedback.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    val focusRequester = remember {
+        FocusRequester()
+    }
+
+    val interactionSource = remember {
+        MutableInteractionSource()
+    }
+
+    val isFocused by interactionSource.collectIsFocusedAsState()
+
+    var searchText by remember {
+        mutableStateOf("")
+    }
+
+    val categoryList by remember {
+        mutableStateOf(
+            categories.map { category ->
+                Pair(category, getHighlightedString(category.name, ""))
+            }
+        )
+    }
+
+    var filteredCategoryList by remember {
+        mutableStateOf(categoryList)
+    }
 
     val listState = rememberLazyListState()
 
     LaunchedEffect(Unit) {
         listState.scrollToItem(index)
+    }
+
+    LaunchedEffect(key1 = searchText) {
+        filteredCategoryList = if (searchText.isBlank()) {
+            categoryList
+        } else {
+            categoryList
+                .filter {
+                    it.first.name.contains(
+                        searchText,
+                        ignoreCase = true
+                    )
+                }
+                .map {
+                    Pair(it.first, getHighlightedString(it.first.name, searchText))
+                }
+        }
     }
 
     Column(
@@ -56,26 +115,57 @@ fun SelectCategoryPopup(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
+        Row(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp))
+        ) {
+            OutlinedTextField(
+                value = searchText,
+                onValueChange = {
+                    searchText = it
+                },
+                enabled = true,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .focusRequester(focusRequester = focusRequester),
+                label = {
+                    Text(text = stringResource(id = R.string.search_field_label))
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        keyboardController?.hide()
+                    }
+                ),
+                shape = RoundedCornerShape(14.dp),
+                interactionSource = interactionSource
+            )
+        }
         LazyColumn(
             state = listState,
             modifier = Modifier
-                .heightIn(max = 200.dp),
+                .heightIn(max = 230.dp)
+                .animateContentSize(),
             verticalArrangement = Arrangement.spacedBy(8.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             items(
-                items = categories,
-                key = { category -> "category-${category.uuid}" }
-            ) { category ->
-                IconToggleRow(
-                    label = category.name,
-                    icon = category.icon,
-                    selected = category.uuid == selectedCategoryUUID,
+                items = filteredCategoryList,
+                key = { categoryPair -> "category-${categoryPair.first.uuid}" }
+            ) { categoryPair ->
+                SelectableCard(
+                    checked = { categoryPair.first.uuid == selectedCategoryUUID },
+                    label = categoryPair.second,
+                    icon = categoryPair.first.icon,
                     onClick = {
                         haptics.performHapticFeedback(HapticFeedbackType.LongPress)
                         dismiss()
-                        setSelectedCategory(category)
-                    }
+                        setSelectedCategory(categoryPair.first)
+                    },
+                    modifier = Modifier.animateItem()
                 )
             }
         }
@@ -83,19 +173,35 @@ fun SelectCategoryPopup(
             modifier = Modifier
                 .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            OutlinedButton(
+            FilledTonalButton(
                 onClick = {
                     haptics.performHapticFeedback(HapticFeedbackType.LongPress)
                     dismiss()
                 },
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .weight(1f)
                     .height(50.dp),
                 shape = RoundedCornerShape(35)
             ) {
                 Text(text = stringResource(id = R.string.cancel_button_label))
+            }
+            FilledTonalButton(
+                onClick = {
+                    haptics.performHapticFeedback(HapticFeedbackType.LongPress)
+                    if (isFocused) {
+                        keyboardController?.show()
+                    } else {
+                        focusRequester.requestFocus()
+                    }
+                },
+                modifier = Modifier
+                    .weight(1f)
+                    .height(50.dp),
+                shape = RoundedCornerShape(35)
+            ) {
+                Text(text = stringResource(id = R.string.search_field_label))
             }
         }
     }
