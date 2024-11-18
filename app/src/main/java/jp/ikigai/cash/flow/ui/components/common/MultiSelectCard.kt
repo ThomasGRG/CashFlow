7d8a49e58b771307e69dd618af99e17328ff89e1
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2024-11-18 21:28:03 +0530

    Add variant with icon

--- app/src/main/java/jp/ikigai/cash/flow/ui/components/common/MultiSelectCard.kt
+++ app/src/main/java/jp/ikigai/cash/flow/ui/components/common/MultiSelectCard.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
@@ -15,9 +16,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import compose.icons.TablerIcons
+import compose.icons.tablericons.BuildingBank
 
 @Composable
 fun MultiSelectCard(
@@ -50,6 +54,51 @@ fun MultiSelectCard(
     }
 }
 
+@Composable
+fun MultiSelectCard(
+    checked: () -> Boolean,
+    label: AnnotatedString,
+    icon: ImageVector,
+    iconTint: Color = MaterialTheme.colorScheme.onBackground,
+    onClick: () -> Unit,
+    modifier: Modifier
+) {
+    OutlinedCard(
+        onClick = onClick,
+        colors = CardDefaults.outlinedCardColors(
+            containerColor = Color.Transparent
+        ),
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(14.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(6.dp),
+                modifier = Modifier.weight(1f, fill = false)
+            ) {
+                Checkbox(checked = checked(), onCheckedChange = null)
+                Text(
+                    text = label,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    modifier = Modifier.padding(end = 10.dp)
+                )
+            }
+            Icon(
+                imageVector = icon,
+                contentDescription = icon.name,
+                tint = iconTint
+            )
+        }
+    }
+}
+
 @Preview
 @Composable
 fun MultiSelectCardPreview() {
@@ -66,6 +115,13 @@ fun MultiSelectCardPreview() {
             modifier = Modifier
         )
         MultiSelectCard(
+            checked = { true },
+            label = AnnotatedString("Test"),
+            icon = TablerIcons.BuildingBank,
+            onClick = {},
+            modifier = Modifier
+        )
+        MultiSelectCard(
             checked = { false },
             label = AnnotatedString("Test aklnd enui ioaend uaenf ioawndu aebfg"),
             onClick = {},
@@ -74,6 +130,7 @@ fun MultiSelectCardPreview() {
         MultiSelectCard(
             checked = { false },
             label = AnnotatedString("Test oiadnu uanei ounbif ounae oinuf jdfih gfouni"),
+            icon = TablerIcons.BuildingBank,
             onClick = {},
             modifier = Modifier
         )
