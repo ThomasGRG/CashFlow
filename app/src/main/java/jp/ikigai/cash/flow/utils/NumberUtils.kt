a930a8ca6d771c593e0cae8a4eeb786883c98f83
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2024-11-19 16:29:59 +0530

    Add currency formatter

--- app/src/main/java/jp/ikigai/cash/flow/utils/NumberUtils.kt
+++ app/src/main/java/jp/ikigai/cash/flow/utils/NumberUtils.kt
@@ -4,6 +4,7 @@ import android.icu.number.LocalizedNumberFormatter
 import android.icu.number.Notation
 import android.icu.number.NumberFormatter
 import android.icu.number.Precision
+import android.icu.util.Currency
 import java.util.Locale
 
 fun getNumberFormatter(locale: Locale? = null): LocalizedNumberFormatter {
@@ -11,4 +12,10 @@ fun getNumberFormatter(locale: Locale? = null): LocalizedNumberFormatter {
         .withLocale(locale ?: Locale.getDefault())
         .notation(Notation.simple())
         .precision(Precision.maxFraction(2))
+}
+
+fun getCurrencyFormatter(locale: Locale? = null, currencyCode: String): LocalizedNumberFormatter {
+    return NumberFormatter
+        .withLocale(locale ?: Locale.getDefault())
+        .unit(Currency.getInstance(currencyCode))
 }
