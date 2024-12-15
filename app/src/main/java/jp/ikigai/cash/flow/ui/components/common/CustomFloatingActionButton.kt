ff6d6fd478470cef708f269037da1e6912c2ea6a
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2024-12-15 22:47:51 +0530

    Add CustomFloatingActionButton component

---
+++ app/src/main/java/jp/ikigai/cash/flow/ui/components/common/CustomFloatingActionButton.kt
@@ -0,0 +1,68 @@
+package jp.ikigai.cash.flow.ui.components.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomFloatingActionButton(
+    enabled: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    FilledTonalButton(
+        onClick = onClick,
+        enabled = enabled,
+        modifier = modifier.defaultMinSize(minHeight = 56.dp, minWidth = 56.dp),
+        shape = FloatingActionButtonDefaults.shape,
+        colors = ButtonDefaults.filledTonalButtonColors(
+            containerColor = FloatingActionButtonDefaults.containerColor,
+            contentColor = contentColorFor(backgroundColor = FloatingActionButtonDefaults.containerColor)
+        ),
+        elevation = ButtonDefaults.filledTonalButtonElevation(
+            defaultElevation = 6.dp,
+            pressedElevation = 6.dp,
+            focusedElevation = 6.dp,
+            hoveredElevation = 8.dp,
+            disabledElevation = 0.dp
+        ),
+        contentPadding = PaddingValues(0.dp)
+    ) {
+        content()
+    }
+}
+
+@Preview
+@Composable
+fun CustomFloatingActionButtonPreview() {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        modifier = Modifier.background(MaterialTheme.colorScheme.background)
+    ) {
+        CustomFloatingActionButton(enabled = true, onClick = {}) {
+            Icon(imageVector = Icons.Default.Search, contentDescription = "")
+        }
+        CustomFloatingActionButton(enabled = false, onClick = {}) {
+            Icon(imageVector = Icons.Default.Search, contentDescription = "")
+        }
+        FloatingActionButton(onClick = {}) {
+            Icon(imageVector = Icons.Default.Search, contentDescription = "")
+        }
+    }
+}
