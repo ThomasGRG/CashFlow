76a489dbdde2059e0d0449df0add2a0b9c480bd0
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2024-12-15 22:50:06 +0530

    Add selectable TransactionCard component overload

--- app/src/main/java/jp/ikigai/cash/flow/ui/components/cards/TransactionCard.kt
+++ app/src/main/java/jp/ikigai/cash/flow/ui/components/cards/TransactionCard.kt
@@ -1,5 +1,6 @@
 package jp.ikigai.cash.flow.ui.components.cards
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
@@ -8,15 +9,20 @@ import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -119,27 +125,234 @@ fun TransactionCard(
     }
 }
 
-@Preview
+@OptIn(ExperimentalFoundationApi::class, ExperimentalLayoutApi::class)
+@Composable
+fun TransactionCard(
+    checked: Boolean,
+    enabled: Boolean,
+    transactionWithIcons: TransactionWithIcons,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    onLongClick: () -> Unit,
+) {
+    val alpha by remember(key1 = enabled) {
+        mutableFloatStateOf(if (enabled) 1f else 0.38f)
+    }
+
+    val subAlpha by remember(key1 = enabled) {
+        mutableFloatStateOf(if (enabled) 0.6f else 0.38f)
+    }
+
+    val animatedIconColor by animateColorAsState(
+        targetValue = if (enabled) {
+            transactionWithIcons.typeIconColor
+        } else {
+            MaterialTheme.colorScheme.onBackground.copy(0.38f)
+        },
+        label = "animated icon color"
+    )
+
+    ElevatedCard(
+        modifier = modifier
+            .fillMaxWidth()
+            .combinedClickable(
+                onClick = onClick,
+                onLongClick = onLongClick,
+                enabled = enabled
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = transactionWithIcons.annotatedTitle,
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .weight(1f, fill = false)
+                        .alpha(alpha)
+                )
+                Checkbox(
+                    checked = checked,
+                    onCheckedChange = null,
+                    enabled = enabled,
+                    modifier = Modifier.padding(start = 10.dp, top = 5.dp)
+                )
+            }
+            if (transactionWithIcons.annotatedDescription.text.isNotBlank()) {
+                Text(
+                    text = transactionWithIcons.annotatedDescription,
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .alpha(subAlpha)
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(6.dp)
+            ) {
+                Icon(
+                    imageVector = transactionWithIcons.typeIcon,
+                    contentDescription = "type icon",
+                    tint = animatedIconColor,
+                    modifier = Modifier.size(30.dp)
+                )
+                Text(
+                    text = transactionWithIcons.amount,
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.alpha(alpha)
+                )
+            }
+            FlowRow(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp, alignment = Alignment.Start),
+                verticalArrangement = Arrangement.spacedBy(
+                    5.dp,
+                    alignment = Alignment.CenterVertically
+                )
+            ) {
+                transactionWithIcons.chips.forEach {
+                    FilledTonalButton(
+                        onClick = onClick,
+                        enabled = enabled,
+                        shape = MaterialTheme.shapes.small,
+                        contentPadding = PaddingValues(vertical = 8.dp, horizontal = 12.dp)
+                    ) {
+                        Icon(
+                            imageVector = it.icon,
+                            contentDescription = it.icon.name,
+                            modifier = Modifier.padding(end = 6.dp)
+                        )
+                        Text(
+                            text = stringResource(id = it.resId, it.value),
+                            style = MaterialTheme.typography.titleMedium
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(device = "spec:width=2340px,height=2340px,dpi=440")
 @Composable
 fun TransactionCardPreview() {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(10.dp)
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
     ) {
-        TransactionCard(
-            transactionWithIcons = TransactionWithIcons(
-                annotatedTitle = AnnotatedString("Test"),
-                annotatedDescription = AnnotatedString("Desc"),
-                amount = "423.09",
-                chips = listOf(
-                    ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
-                    ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
-                    ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
-                    ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+        Column(
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(0.5f)
+        ) {
+            TransactionCard(
+                checked = false,
+                enabled = true,
+                transactionWithIcons = TransactionWithIcons(
+                    annotatedTitle = AnnotatedString("Test askjdef anedkufb awindas uifb urnit"),
+                    annotatedDescription = AnnotatedString("Desc"),
+                    amount = "423.09",
+                    chips = listOf(
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                    ),
                 ),
-            ),
-            onClick = {},
-            onLongClick = {},
-            modifier = Modifier.fillMaxWidth()
-        )
+                onClick = {},
+                onLongClick = {},
+                modifier = Modifier.fillMaxWidth()
+            )
+            TransactionCard(
+                checked = false,
+                enabled = false,
+                transactionWithIcons = TransactionWithIcons(
+                    annotatedTitle = AnnotatedString("Test askjdef anedkufb awindas uifb urnit"),
+                    annotatedDescription = AnnotatedString("Desc"),
+                    amount = "423.09",
+                    chips = listOf(
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                    ),
+                ),
+                onClick = {},
+                onLongClick = {},
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        Column(
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            TransactionCard(
+                transactionWithIcons = TransactionWithIcons(
+                    annotatedTitle = AnnotatedString("Test"),
+                    annotatedDescription = AnnotatedString("Desc"),
+                    amount = "423.09",
+                    chips = listOf(
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                    ),
+                ),
+                onClick = {},
+                onLongClick = {},
+                modifier = Modifier.fillMaxWidth()
+            )
+            TransactionCard(
+                checked = true,
+                enabled = true,
+                transactionWithIcons = TransactionWithIcons(
+                    annotatedTitle = AnnotatedString("Test askjdef anedkufb awindas uifb urnit"),
+                    annotatedDescription = AnnotatedString("Desc"),
+                    amount = "423.09",
+                    chips = listOf(
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                    ),
+                ),
+                onClick = {},
+                onLongClick = {},
+                modifier = Modifier.fillMaxWidth()
+            )
+            TransactionCard(
+                checked = true,
+                enabled = false,
+                transactionWithIcons = TransactionWithIcons(
+                    annotatedTitle = AnnotatedString("Test askjdef anedkufb awindas uifb urnit"),
+                    annotatedDescription = AnnotatedString("Desc"),
+                    amount = "423.09",
+                    chips = listOf(
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                        ChipInfo(R.string.placeholder, "Category", TablerIcons.Archive),
+                    ),
+                ),
+                onClick = {},
+                onLongClick = {},
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
     }
 }
