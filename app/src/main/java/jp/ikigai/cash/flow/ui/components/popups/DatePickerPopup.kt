509fd038dd5abf2c2a31a833dacc7ff59fb6a88a
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2024-12-26 15:48:07 +0530

    Fix transparent background for year selection view

--- app/src/main/java/jp/ikigai/cash/flow/ui/components/popups/DatePickerPopup.kt
+++ app/src/main/java/jp/ikigai/cash/flow/ui/components/popups/DatePickerPopup.kt
@@ -24,7 +24,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.stringResource
@@ -62,7 +61,7 @@ fun DatePickerPopup(
         DatePicker(
             state = datePickerState,
             colors = DatePickerDefaults.colors(
-                containerColor = Color.Transparent
+                containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
             )
         )
         Row(
