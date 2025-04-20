9b3963c4d2d7dbbdbf6d2f8054d81f40b7c56dd4
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2025-04-20 09:05:37 +0530

    Add WaitDialog

---
+++ app/src/main/java/jp/ikigai/cash/flow/ui/components/common/WaitDialog.kt
@@ -0,0 +1,59 @@
+package jp.ikigai.cash.flow.ui.components.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
+import jp.ikigai.cash.flow.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WaitDialog() {
+    BasicAlertDialog(
+        onDismissRequest = {},
+        properties = DialogProperties(
+            dismissOnClickOutside = false,
+            dismissOnBackPress = false
+        ),
+        modifier = Modifier
+            .background(
+                color = MaterialTheme.colorScheme.surfaceContainerHigh,
+                shape = MaterialTheme.shapes.extraLarge
+            )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(20.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            CircularProgressIndicator()
+            Text(
+                text = stringResource(R.string.please_wait_dialog_label),
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun WaitDialogPreview() {
+    WaitDialog()
+}
