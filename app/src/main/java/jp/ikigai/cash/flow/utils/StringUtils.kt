f8141adb6317ef4a3e591f13d4848ceafe8a0681
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2025-05-04 20:36:21 +0530

    Remove get icon extensions for CounterParty, Method and Source

--- app/src/main/java/jp/ikigai/cash/flow/utils/StringUtils.kt
+++ app/src/main/java/jp/ikigai/cash/flow/utils/StringUtils.kt
@@ -16,18 +16,6 @@ fun String.getIconForCategory(): ImageVector {
     return TablerIcons.AllIcons.find { it.name == this } ?: Constants.DEFAULT_CATEGORY_ICON
 }
 
-fun String.getIconForCounterParty(): ImageVector {
-    return TablerIcons.AllIcons.find { it.name == this } ?:Constants.DEFAULT_COUNTERPARTY_ICON
-}
-
-fun String.getIconForMethod(): ImageVector {
-    return TablerIcons.AllIcons.find { it.name == this } ?:Constants.DEFAULT_METHOD_ICON
-}
-
-fun String.getIconForSource(): ImageVector {
-    return TablerIcons.AllIcons.find { it.name == this } ?:Constants.DEFAULT_SOURCE_ICON
-}
-
 fun getHighlightedString(searchString: String, highlightText: String): AnnotatedString {
     return buildAnnotatedString {
         val startIndex = searchString.indexOf(
