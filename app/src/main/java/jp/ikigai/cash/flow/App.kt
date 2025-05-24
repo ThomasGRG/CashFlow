a1ce5adcc7a0ef6f98448f6a2f1c052708eb800a
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2025-05-24 10:36:32 +0530

    Create and init DataStore for objectbox

--- app/src/main/java/jp/ikigai/cash/flow/App.kt
+++ app/src/main/java/jp/ikigai/cash/flow/App.kt
@@ -1,6 +1,7 @@
 package jp.ikigai.cash.flow
 
 import android.app.Application
+import jp.ikigai.cash.flow.data.store.DataStore
 import jp.ikigai.cash.flow.koin.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -10,6 +11,7 @@ class App: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        DataStore.init(this)
         startKoin {
             androidLogger()
             androidContext(this@App)
