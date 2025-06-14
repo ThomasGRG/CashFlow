000c66d183db55111aa331103e9b3ad522813481
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2025-06-14 18:04:30 +0530

    Add SearchBox component

---
+++ app/src/main/java/jp/ikigai/cash/flow/ui/components/common/SearchBox.kt
@@ -0,0 +1,85 @@
+package jp.ikigai.cash.flow.ui.components.common
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import jp.ikigai.cash.flow.R
+
+@Composable
+fun SearchBox(
+    modifier: Modifier = Modifier,
+    searchText: String,
+    setSearchText: (String) -> Unit,
+    enabled: Boolean = true,
+    focusRequester: FocusRequester,
+    interactionSource: MutableInteractionSource
+) {
+    val haptics = LocalHapticFeedback.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    OutlinedTextField(
+        value = searchText,
+        onValueChange = setSearchText,
+        enabled = enabled,
+        modifier = modifier
+            .fillMaxWidth()
+            .focusRequester(focusRequester = focusRequester),
+        label = {
+            Text(text = stringResource(id = R.string.search_field_label))
+        },
+        trailingIcon = {
+            AnimatedVisibility(
+                visible = enabled && searchText.isNotEmpty(),
+                enter = scaleIn() + fadeIn(),
+                exit = scaleOut() + fadeOut()
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Clear,
+                    contentDescription = "clear field",
+                    modifier = Modifier
+                        .clickable(
+                            enabled = enabled,
+                            onClick = {
+                                haptics.performHapticFeedback(HapticFeedbackType.LongPress)
+                                setSearchText("")
+                            }
+                        )
+                )
+            }
+        },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                keyboardController?.hide()
+            }
+        ),
+        shape = RoundedCornerShape(14.dp),
+        interactionSource = interactionSource
+    )
+}
