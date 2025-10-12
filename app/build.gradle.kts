99f2a686d371709e5f061b8c98ca59393a422975
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2025-10-12 09:39:52 +0530

    Add compose adaptive dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.3")
+    implementation("androidx.compose.material3.adaptive:adaptive:1.1.0")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation(platform("androidx.compose:compose-bom:2025.09.00"))
     implementation("androidx.compose.ui:ui")
