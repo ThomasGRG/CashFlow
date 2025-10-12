47cd885da65475546f825c4a06f5ff1ba31d9e73
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2025-10-12 12:59:53 +0530

    Add SettingsScreenBottomAppBar component

---
+++ app/src/main/java/jp/ikigai/cash/flow/ui/components/bottombars/SettingsScreenBottomAppBar.kt
@@ -0,0 +1,99 @@
+package jp.ikigai.cash.flow.ui.components.bottombars
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SettingsScreenBottomAppBar(
+    navigateBack: () -> Unit,
+) {
+    val haptics = LocalHapticFeedback.current
+
+    BottomAppBar(
+        modifier = Modifier.padding(top = 10.dp),
+        contentPadding = PaddingValues(4.dp)
+    ) {
+        IconButton(
+            onClick = {
+                haptics.performHapticFeedback(HapticFeedbackType.LongPress)
+                navigateBack()
+            },
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = "navigate back"
+            )
+        }
+    }
+}
+
+@Composable
+fun SettingsScreenBottomAppBar(
+    title: String,
+    subTitle: String,
+    navigateBack: () -> Unit,
+) {
+    val haptics = LocalHapticFeedback.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(
+                RoundedCornerShape(20.dp)
+            )
+            .background(MaterialTheme.colorScheme.surfaceContainer)
+            .padding(all = 10.dp)
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleLarge,
+        )
+        Text(
+            text = subTitle,
+            style = MaterialTheme.typography.titleSmall,
+            modifier = Modifier.alpha(0.8f)
+        )
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 10.dp)
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+        ) {
+            IconButton(
+                onClick = {
+                    haptics.performHapticFeedback(HapticFeedbackType.LongPress)
+                    navigateBack()
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "navigate back"
+                )
+            }
+        }
+    }
+}
