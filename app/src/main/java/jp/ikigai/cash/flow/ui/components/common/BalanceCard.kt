6a99cfca3c7718eaa4b9133f6d726228fab05133
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2025-12-09 09:09:22 +0530

    BalanceCard - Show total balance header

--- app/src/main/java/jp/ikigai/cash/flow/ui/components/common/BalanceCard.kt
+++ app/src/main/java/jp/ikigai/cash/flow/ui/components/common/BalanceCard.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import jp.ikigai.cash.flow.R
 
 @Composable
 fun BalanceCard(
@@ -22,10 +24,14 @@ fun BalanceCard(
             .fillMaxWidth()
             .padding(top = 10.dp),
     ) {
+        Text(
+            text = stringResource(id = R.string.total_balance_label),
+            modifier = Modifier.padding(top = 10.dp, start = 10.dp),
+        )
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(all = 10.dp),
+                .padding(start = 10.dp, bottom = 10.dp, end = 10.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start,
         ) {
