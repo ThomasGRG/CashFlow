ac726889ff3eb30e275a6b944e7e7b093cd70a54
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2025-12-24 20:44:59 +0530

    Release v3.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "jp.ikigai.cash.flow"
         minSdk = 30
         targetSdk = 35
-        versionCode = 26
-        versionName = "3.4.0"
+        versionCode = 27
+        versionName = "3.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
